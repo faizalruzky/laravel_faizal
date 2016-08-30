22280b854228a72453c2a5f387faeda45580b1c1
faizalruzky <faizalruzky@gmail.com>
2016-08-30 14:23:49 +0700

    tambah source custom.js

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -14,58 +14,5 @@
   <div id="articles-list">
  @include('articles.list')
 </div>
-<script type="text/javascript">
-  $(function(){
-  $.ajaxSetup({
-    headers: {
-      'X-XSRF-Token': $('meta[name="_token"]').attr('content')
-    }
-  });
-});
-$(document).ready(function() {
-
-  $(document).on('click', '.pagination a', function(e) {
-
-    get_page($(this).attr('href').split('page=')[1]);
-
-    e.preventDefault();
-
-  });
-
-});
-
-function get_page(page) {
-
-  $.ajax({
-
-    url : '/articles?page=' + page,
-
-    type : ‘GET’,
-
-    dataType : 'json',
-
-    success : function(data) {
-      console.log(data);
-
-      $('#articles-list').html(data['view']);
-
-    },
-
-    error : function(xhr, status, error) {
-
-      console.log(xhr.error + "\n ERROR STATUS : " + status + "\n" + error);
-
-    },
-
-    complete : function() {
-
-      alreadyloading = false;
-
-    }
-
-  });
-
-}
-</script>
- 
+ <script src="/js/custom.js"></script>
 @stop
