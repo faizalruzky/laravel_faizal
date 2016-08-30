98bfd85ace689a84a552a63b5784eeed2a1d6c46
faizalruzky <faizalruzky@gmail.com>
2016-08-30 16:45:24 +0700

    hapus source search.js

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -11,8 +11,37 @@
       <i class="fa fa-plus"></i> Create Article
     </a>
   </div>
-  <div id="articles-list">
- @include('articles.list')
-</div>
+  <div class="row">
+
+  <div class="col-md-12 search">
+
+    <div class="col-md-6">
+
+      <div class="input-group input-group-sm">
+
+        <input type="text" class="form-control" id="keywords" placeholder="Keywords">
+
+        <span class="input-group-btn">
+
+        <button id="search" class="btn btn-info btn-flat" type="button">
+
+          Go!
+
+        </button>
+
+        </span>
+
+      </div><!-- /input-group -->
+
+    </div>
+
+  </div>
+
+ </div>
  <script src="/js/custom.js"></script>
+  <div id="articles-list">
+ 
+ @include('articles.list')
+ </div>
+
 @stop
