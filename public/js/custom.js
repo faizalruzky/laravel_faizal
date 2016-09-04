59d608c5ab8f26804739b1a6d0472cb18fc9e722
faizalruzky <faizalruzky@gmail.com>
2016-09-04 21:04:20 +0700

    tambah js sorting

--- public/js/custom.js
+++ public/js/custom.js
@@ -87,3 +87,69 @@ $('#search').on('click', function(){
 
 });
 
+$(document).ready(function() {
+
+  $(document).on('click', '#id', function(e) {
+
+    sort_articles();
+
+    e.preventDefault();
+
+  });
+
+});
+
+function sort_articles() {
+
+  $('#id').on('click', function() {
+
+    $.ajax({
+
+      url : '/articles',
+
+      typs : 'GET',
+
+      dataType : 'json',
+
+      data : {
+
+        "direction" : $('#direction').val()
+
+      },
+
+      success : function(data) {
+
+        $('#articles-list').html(data['view']);
+
+        $('#direction').val(data['direction']);
+
+        if(data['direction'] == 'asc') {
+
+          $('i#ic-direction').attr({class: "fa fa-arrow-up"});
+
+        } else {
+
+          $('i#ic-direction').attr({class: "fa fa-arrow-down"});
+
+        }
+
+      },
+
+      error : function(xhr, status, error) {
+
+        console.log(xhr.error + "\n ERROR STATUS : " + status + "\n" + error);
+
+      },
+
+      complete : function() {
+
+        alreadyloading = false;
+
+      }
+
+    });
+
+  });
+
+}
+
